8fbedcb09315f0f5e4baba047fca0da73ebeaa89
FlynntKnapp <bruce.stull@gmail.com>
2023-12-13 08:27:02 -0500

    Add tests

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RewardValueTests {
 
@@ -8,7 +9,7 @@ public class RewardValueTests {
     void create_with_cash_value() {
         double cashValue = 100;
         var rewardValue = new RewardValue(cashValue);
-        assertEquals(cashValue, rewardValue.getCashValue());
+        assertEquals(cashValue, rewardValue.getCashValue(), 0.01);
     }
 
     @Test
@@ -20,11 +21,20 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 100;
+        var rewardValue = new RewardValue(cashValue);
+        double expectedMilesValue = Math.round(cashValue / .0035 * 100.0f) / 100.0f;
+        // epsilon of 0.01 is set for floating point comparison
+        assertTrue(Math.abs(expectedMilesValue - rewardValue.getMilesValue()) < 0.01);
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 10000;
+        var rewardValue = new RewardValue(milesValue);
+        double expectedCashValue = milesValue * .0035;
+        // epsilon of 0.01 is set for floating point comparison
+        assertTrue(Math.abs(expectedCashValue - rewardValue.getCashValue()) < 0.01);
+
     }
 }
