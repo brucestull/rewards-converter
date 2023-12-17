6284739bf37abc95d493703fd60d2045227e5ed6
FlynntKnapp <bruce.stull@gmail.com>
2023-12-17 12:23:00 -0500

    Changed 'input_value' to camel-case 'inputValue'.

--- src/main/java/RewardsConverter.java
+++ src/main/java/RewardsConverter.java
@@ -5,16 +5,29 @@ public class RewardsConverter {
         var scanner = new Scanner(System.in);
         System.out.println("Welcome to the Credit Card Rewards Converter!");
         System.out.println("Please enter a cash value to convert to airline miles: ");
-        var input_value = scanner.nextLine();
+        var inputValue = scanner.nextLine();
         double cashValue;
         try {
-            cashValue = Double.parseDouble(input_value);
+            cashValue = Double.parseDouble(inputValue);
         } catch (NumberFormatException exception) {
             System.out.println("Could not parse input value as a double, exiting");
             return;
         }
-        System.out.println("converting $" + input_value + " to miles");
-        var rewardsValue = new RewardValue(cashValue);
-        System.out.println("$" + input_value + " is worth " + rewardsValue.getMilesValue() + " miles");
+        System.out.println("converting $" + inputValue + " to miles");
+        var rewardsValueFromCash = new RewardValue(cashValue);
+        System.out.println("$" + inputValue + " is worth " + rewardsValueFromCash.getMilesValue() + " miles");
+        // Convert miles to cash:
+        System.out.println("Please enter an airline miles value to convert to cash: ");
+        inputValue = scanner.nextLine();
+        int milesValue;
+        try {
+            milesValue = Integer.parseInt(inputValue);
+        } catch (NumberFormatException exception) {
+            System.out.println("Could not parse input value as a double, exiting");
+            return;
+        }
+        System.out.println("converting " + inputValue + " to cash");
+        RewardValue rewardsValueFromMiles = new RewardValue(milesValue);
+        System.out.println(inputValue + " miles is worth $" + rewardsValueFromMiles.getCashValue());
     }
 }
