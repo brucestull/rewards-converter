cdb47e9b13138d31368017cb03f959001751d589
FlynntKnapp <bruce.stull@gmail.com>
2023-12-17 13:06:40 -0500

    Add Javadocs

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -3,21 +3,46 @@
  * <p>
  * RewardValue can be created using either a cash value or a miles value.
  * It provides methods to retrieve these values and perform conversions between them.
+ * Only the `cashValue` will be stored in the object. Conversions will be used to calculate any `milesValue`s.
  */
 public class RewardValue {
-    private double miles;
-    private double cash;
-    public RewardValue(int miles) {
-        // Set miles to input value:
-        this.miles = miles;
-        // Convert miles to dollars:
-        this.cash = miles * .0035;
+    // Use `final` since `RewardValue` will not change. This is one specific instance of a `RewardValue`:
+    private final double cashValue;
+    // Add a constant to hold the conversion rate:
+    public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
+
+    /**
+     * This is a constructor which saves the submitted cash value in the object.
+     *
+     */
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
     }
-    public RewardValue(double cash) {
-        // Convert dollars to cents, convert cents to miles, restore decimal to proper place:
-        this.miles = Math.round(cash * 100.0f / .0035) / 100.0f;
-        // Convert dollars to cents, round to nearest cent, convert to dollars:
-        this.cash = Math.round(cash * 100.0f) / 100.0f;
+
+    /**
+     * This is a constructor which converts submitted miles value to a cash value and saves it in the object.
+     *
+     */
+    public RewardValue(int milesValue) {
+        this.cashValue = convertToCash(milesValue);
+    }
+
+    /**
+     * This method returns the miles value of cash value.
+     *
+     * @return An integer representing the miles value.
+     */
+    private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
+    }
+
+    /**
+     * This method returns the cash value of miles value.
+     *
+     * @return A double representing the cash value.
+     */
+    private static double convertToCash(int milesValue) {
+        return milesValue * MILES_TO_CASH_CONVERSION_RATE;
     }
 
     /**
@@ -26,7 +51,7 @@ public class RewardValue {
      * @return A double representing the cash value.
      */
     public double getCashValue() {
-        return this.cash;
+        return this.cashValue;
     }
 
     /**
@@ -35,6 +60,6 @@ public class RewardValue {
      * @return A double representing the miles value.
      */
     public double getMilesValue() {
-        return this.miles;
+        return convertToMiles(this.cashValue);
     }
 }
