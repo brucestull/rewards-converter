195f94a9384387586ee083b9dae71adaaae58bd0
FlynntKnapp <bruce.stull@gmail.com>
2023-12-18 02:00:51 -0500

    Refactor tests to use class constant 'MILES_TO_CASH_CONVERSION_RATE'.

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -22,19 +22,16 @@ public class RewardValueTests {
     @Test
     void convert_from_cash_to_miles() {
         double cashValue = 100;
+        int expectedMilesValue = (int) (cashValue / RewardValue.MILES_TO_CASH_CONVERSION_RATE);
         var rewardValue = new RewardValue(cashValue);
-        double expectedMilesValue = Math.round(cashValue / .0035);
-        // epsilon of 0.01 is set for floating point comparison
-        assertTrue(Math.abs(expectedMilesValue - rewardValue.getMilesValue()) < 0.01);
+        assertEquals(expectedMilesValue, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
         int milesValue = 10000;
+        double expectedCashValue = milesValue * RewardValue.MILES_TO_CASH_CONVERSION_RATE;
         var rewardValue = new RewardValue(milesValue);
-        double expectedCashValue = milesValue * .0035;
-        // epsilon of 0.01 is set for floating point comparison
-        assertTrue(Math.abs(expectedCashValue - rewardValue.getCashValue()) < 0.01);
-
+        assertEquals(expectedCashValue, rewardValue.getCashValue());
     }
 }
